355106c4d7e31cb709167dbecad75fc3b7fb8a9a
Alexis Yannuzzi <alexisy@turboserver.com.ar>
2021-09-14 18:59:48 -0300

    Setup ASP.NET Core project and baseline dependencies.

---
+++ Minesweeper/Dto/CreateGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Dto
+{
+    public class CreateGame
+    {
+        public Size Field { get; set; }
+        public ushort Mines { get; set; }
+        public Point Move { get; set; }
+    }
+}
---
+++ Minesweeper/Dto/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Minesweeper.Dto
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+
+        public Size Field { get; set; }
+
+        public IDictionary<Point, byte> Uncovered { get; set; }
+    }
+}
